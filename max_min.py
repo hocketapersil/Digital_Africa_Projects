91e0b256d493a291b71da81c874766cbe5e52b65
hocketapersil <hocketapersil@gmail.com>
2024-05-07 13:54:21 +0000

    Added the max_min file

---
+++ max_min.py
@@ -0,0 +1,7 @@
+def max_min(data):
+    max_val = max(data)
+    min_val = min(data)
+    return (max_val, min_val)
+
+# Path: digital_africa_git_ex5/test_max_min.py
+print(max_min([1, 2, 3, 4, 5])) 
